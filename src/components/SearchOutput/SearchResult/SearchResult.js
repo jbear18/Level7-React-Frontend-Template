2d8b0dd05cd6ba6961292137aa541ef85b0a3da7
olive21221 <orp7931@icloud.com>
2022-07-30 11:30:27 -0700

    work on embedding image & link

--- src/components/SearchOutput/SearchResult/SearchResult.js
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -5,11 +5,24 @@ const SearchResult = (props) => {
     console.log(props);
     return (
 
-    <article className={classes.Result}>
-        <h1>Recipe Title: {props.title}</h1>
-        <h3>Calories: {props.calories}</h3>
-        <h3>Image: {props.image}</h3>
-        </article>
+//    <article className={classes.Result}>
+//        <h1>Recipe Title: {props.title}</h1>
+//        <h3>Image: {props.image}</h3>
+//        </article>
+
+<article className={classes.Result}>
+    <h1>Title: {props.title}</h1>
+    <h3>Image: {props.image}</h3>
+    <h3 className={classes.Result}>
+        {props.results && props.results.map((title, index) => (
+            <p key={index}>{title}</p>
+//            <p link="https://spoonacular.com/" + props.title.split(" ")[props.title.split(" ").length-1] + "-" + "props.id">"hello"</p>
+        ))}
+        </h3>
+        <a href={props.link} rel="noopener noreferrer" target="_blank">{props.link}</a>
+</article>
+
+
     );
 }
 
