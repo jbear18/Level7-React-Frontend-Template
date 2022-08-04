3ed1c3258296bb9b9b1d02f99e1b821ce15a0bde
olive21221 <orp7931@icloud.com>
2022-08-03 20:01:18 -0700

    finished

--- src/components/SearchOutput/SearchResult/SearchResult.js
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -5,22 +5,11 @@ const SearchResult = (props) => {
     console.log(props);
     return (
 
-//    <article className={classes.Result}>
-//        <h1>Recipe Title: {props.title}</h1>
-//        <h3>Image: {props.image}</h3>
-//        </article>
-
 <article className={classes.Result}>
     <h1>Title: {props.title}</h1>
-    <h3>Image: {props.image}</h3>
-    <h3 className={classes.Result}>
-        {props.results && props.results.map((title, index) => (
-            <p key={index}>{title}</p>
-
-        ))}
-        </h3>
-        <script> props.link="https://spoonacular.com/" + props.title.split(" ")[props.title.split(" ").length-1] + "-" + "props.id">"hello"</script>
-        <a href={props.link} rel="noopener noreferrer" target="_blank">{props.link}</a>
+    <img src={props.image} alt={props.title}></img>
+    <h3></h3>
+ <a href={props.link} rel="noopener noreferrer" target="_blank">{props.link}</a>
 </article>
 
 
