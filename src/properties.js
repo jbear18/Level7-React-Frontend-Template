48e32ab349b09c087c3891e2a2fa6f3f2f76d26a
jessieshen <shehu01@hotmail.com>
2022-09-08 18:04:39 -0700

    header changes

--- src/properties.js
+++ src/properties.js
@@ -1,10 +1,10 @@
 export const properties = {
     courseName: 'Recipe Search',
-    endpoint: 'http://localhost:5000/searchSpoonacularResults?q=',
-//    endpoint: 'https://recipe-search-one.vercel.app/searchSpoonacularResults?q=',
+//    endpoint: 'http://localhost:5000/searchSpoonacularResults?q=',
+    endpoint: 'https://recipe-search-one.vercel.app/searchSpoonacularResults?q=',
     description: 'Search for a recipe or discover a new recipe! Input a search term and a result of the recipe, image, #calories, and ID will pop up.',
-    backendSwaggerUrl: 'http://localhost:5000/',
-//    backendSwaggerUrl: 'https://recipe-search-one.vercel.app/',
+//    backendSwaggerUrl: 'http://localhost:5000/',
+    backendSwaggerUrl: 'https://recipe-search-one.vercel.app/',
     createdBy: 'Jessie Shen',
     repositoryUrl: 'https://ppjcqy0ktq0.typeform.com/to/Jmj9xECp'
 
